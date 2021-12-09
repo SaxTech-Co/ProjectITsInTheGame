0bd9823d796720d74eed74e1c49dfe85fbbb627c
NickVdPeppel <n.vandepeppel@hotmail.nl>
2021-12-09 10:43:56 +0100

    IntelliJ Edit

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,24 @@
 import nl.saxion.app.SaxionApp;
 
+import java.awt.*;
+
 public class Application implements Runnable {
 
     public static void main(String[] args) {
-        SaxionApp.start(new Application(), 500, 500);
+        SaxionApp.start(new Application(), 750, 600);
     }
 
     public void run() {
-        SaxionApp.printLine("Test");
-
+        //TODO:
+//        SaxionApp.turnBorderOff();
+        SaxionApp.drawRectangle(0,100,750,500);
+        SaxionApp.drawLine(0,150,750,150); //top
+        SaxionApp.drawLine(0,550,750,550); //bottom
+        SaxionApp.drawLine(50, 0, 50, 600); // left
+        SaxionApp.drawLine(700, 0, 700, 600); // right
+        SaxionApp.readChar();
+        SaxionApp.setFill(Color.black);
+        SaxionApp.drawRectangle(0,0,750,100);
     }
 
 }
