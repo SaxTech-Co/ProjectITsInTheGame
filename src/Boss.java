bda6691f041be85109ffe78ac8d786f7406a7089
NickVdPeppel <n.vandepeppel@hotmail.nl>
2021-12-21 12:17:13 +0100

    Added Classes "Taco" and "Boss"
    Properties:
    - name
    - generalHp (Taco Only)
    - combatHp
    - image (with correct path for Taco's image)

---
+++ src/Boss.java
@@ -0,0 +1,11 @@
+public class Boss {
+    String name;
+    int combatHp;
+    String image;
+
+    public Boss(String name, int combatHp, String image) {
+        this.name = name;
+        this.combatHp = combatHp;
+        this.image = image;
+    }
+}
---
+++ src/Taco.java
@@ -0,0 +1,6 @@
+public class Taco {
+    String name = "Taco";
+    int generalHp = 4;
+    int combatHp = 60;
+    String image = "media/graphics/characters/TacoV4Default.png";
+}
